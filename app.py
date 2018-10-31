6d7e7176038ca7d3ebd8c22466b37a33d8dc791c
moiseichyk93@gmail.com <moiseichyk93@gmail.com>
2018-10-31 21:32:01 +0200

    add test

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ from models import (
 )
 from utils import login_required
 
-engine = create_engine(
-    'mysql+mysqlconnector://itea:itea@localhost/itea')
-Session = sessionmaker(bind=engine)
-session = Session()
 
 app = Flask(__name__)
 
@@ -27,7 +23,7 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        user = session.query(User).filter(User.email == email,
+        user = app.session.query(User).filter(User.email == email,
                                           User.password == password).one_or_none()
         
         if user:
@@ -38,18 +34,17 @@ def login():
    
 
 @app.route('/departments', methods = ['GET', 'POST'])
-@login_required
 def departments():
     if request.method == 'POST':
-        if not session.query(Department).filter(Department.city == request.form['city']).one_or_none():
+        if not app.session.query(Department).filter(Department.city == request.form['city']).one_or_none():
             city = request.form['city']
             count_of_workers = request.form['count_of_workers']
             new_department = Department(city = city, count_of_workers = count_of_workers)
-            session.add(new_department)
-            session.commit()
+            app.session.add(new_department)
+            app.session.commit()
         
 
-    departments = session.query(Department).all()
+    departments = app.session.query(Department).all()
 
     return render_template('tables.html', departments = departments)
 
@@ -58,11 +53,11 @@ def departments():
 @login_required
 def applications(application_id):
     if application_id:
-        application = session.query(Application).filter(Application.id == application_id).one_or_none()
-        session.delete(application)
-        session.commit()
+        application = app.session.query(Application).filter(Application.id == application_id).one_or_none()
+        app.session.delete(application)
+        app.session.commit()
         
-    applications = session.query(Application).all()
+    applications = app.session.query(Application).all()
 
     return render_template('applications.html', applications = applications)
 
@@ -78,10 +73,10 @@ def departments_detail(department_id = None):
         age = request.form['age']
         new_client = Client(department_id = department_id, first_name = first_name, last_name = last_name, education = education, passport = passport, 
                                         city = city, age = age)
-        session.add(new_client)
-        session.commit()
+        app.session.add(new_client)
+        app.session.commit()
 
-    clients = session.query(Department).filter(Department.id == department_id).first().clients
+    clients = app.session.query(Department).filter(Department.id == department_id).first().clients
 
     return render_template('departments_details.html', clients = clients)
 
@@ -101,11 +96,11 @@ def delete_client(department_id = None):
         delete_client = Client(department_id = department_id, first_name = first_name, last_name = last_name, education = education, passport = passport, 
                                         city = city, age = age)
         
-        session.delete(delete_client)
-        session.commit()
+        app.session.delete(delete_client)
+        app.session.commit()
 
 
-    clients = session.query(Department).filter(Department.id == department_id).first().clients
+    clients = app.session.query(Department).filter(Department.id == department_id).first().clients
 
     return render_template('delete_client.html', clients = clients)
 
@@ -115,26 +110,33 @@ def delete_client(department_id = None):
 def register():
     if request.method == 'POST':
         
-        if not session.query(User).filter(User.email == request.form['email']).one_or_none():
+        if not app.session.query(User).filter(User.email == request.form['email']).one_or_none():
             login = request.form['login']
             password = request.form['password']
             email = request.form['email']
             add_user = User(login = login, password = password, 
                                         email = email)
-            session.add(add_user)
-            session.commit()
+            app.session.add(add_user)
+            app.session.commit()
             return redirect('/login')
 
         
     return render_template('register.html')
 
-
-
-if __name__=='__main__':
-    
+def main():
+    engine = create_engine(
+    'mysql+mysqlconnector://itea:itea@localhost/itea')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+        
     Base.metadata.create_all(engine)
+
+    app.session = session
     app.secret_key = b'secret key'
     #http_server = WSGIServer(('', 5000), app)
     #http_server.serve_forever()
 
     app.run(host='0.0.0.0', port=5000, debug = True)
+
+if __name__=='__main__':
+    main()
