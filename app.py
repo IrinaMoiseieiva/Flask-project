11f5c484c462ca80faae7dc0eca130aeb7885f82
moiseichyk93@gmail.com <moiseichyk93@gmail.com>
2018-12-10 10:20:26 +0200

    Changed port to 80

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def main():
     #http_server = WSGIServer(('', 5000), app)
     #http_server.serve_forever()
 
-    app.run(host='0.0.0.0', port=5000, debug = True)
+    app.run(host='0.0.0.0', port=80, debug = True)
 
 if __name__=='__main__':
     main()
